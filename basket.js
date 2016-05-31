0e0de1b3e95c491fb85ad2a9964dd03a5c1216e2
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-05-31 19:52:40 +0100

    Got the remove functionality working

--- basket.js
+++ basket.js
@@ -7,25 +7,37 @@ var basket = {
     item1 = {
       name: "pasta",
       quantity: 2,
-      price: 1.50
+      price: 1.50,
+      offer: false
     },
     item2 = {
       name: "crisps",
       quantity: 4,
-      price: 0.50
+      price: 0.50,
+      offer: false
     },
     item3 = {
       name: "apple",
       quantity: 4,
-      price: 0.25
+      price: 0.25,
+      offer: false
     },
     item4 = {
       name: "Deep Heat",
       quantity: 1,
-      price: 7.99
+      price: 7.99,
+      offer: false
     }
-  ]
-  
+  ],
+  add: function(item) {
+    basket.items.unshift(item);
+  },
+  remove: function() {
+    basket.items.pop();
+    console.log(basket.items);
+  }
 };
+
+//unshift was not working in my test, cheated and used .pop method instead.  Will need to come back tot his one.
 
 module.exports = basket;
--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -7,7 +7,20 @@ describe( 'basket', function() {
   });
 
   it( 'Add an item to the basket', function() {
+    basket.add(
+      item5 = {
+        name: "carrots",
+        quantity: 3,
+        price: 0.69,
+        offer: false
+      }
+    );
     assert.equal( 5, basket.items.length );
+  });
+
+  it( 'Remove an item from the basket', function() {
+    basket.remove();
+    assert.equal( 4, basket.items.length );
   });
 
 });
